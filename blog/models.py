4c318a0370daee8de9a1a4b9d5c49819386604bb
Ehsan <eaca89@gmail.com>
2023-01-03 17:19:45 -0500

    4 models are added

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,32 @@
 from django.db import models
-from django.urls import reverse
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True)
+    help_text = 'A label for URL config.'
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31)    
+    slug = models.SlugField()
+    description = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()    
+    website = models.URLField()
+    tag = models.ManyToManyField(Tag)
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
-    body = models.TextField()
+    title = models.CharField(max_length=65)
+    slug = models.SlugField()
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('date published')
+    link = models.URLField()
+    startup = models.ForeignKey(Startup)
