214fe867f40989422f399d6e4a85b6233a7fdae6
Ehsan <eaca89@gmail.com>
2023-01-04 16:17:19 -0500

    model changed

--- blog/models.py
+++ blog/models.py
@@ -5,28 +5,54 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=31, unique=True)
     help_text = 'A label for URL config.'
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
 class Startup(models.Model):
-    name = models.CharField(max_length=31)    
-    slug = models.SlugField()
+    name = models.CharField(max_length=31, db_index=True)    
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
     description = models.TextField()
-    founded_date = models.DateField()
+    founded_date = models.DateField('date founded')
     contact = models.EmailField()    
-    website = models.URLField()
+    website = models.URLField(max_length=255)
     tag = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
+
 class Post(models.Model):
-    title = models.CharField(max_length=65)
-    slug = models.SlugField()
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63, help_text='A label for URL config', unique_for_month='pub_date')
     text = models.TextField()
-    pub_date = models.DateField()
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
+    pub_date = models.DateField('date published', auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
 
     def __str__(self):
-        return self.title
+        return "{} on {}".format(self.title, self.pub_date.strftime('%Y-%m-%d'))
+    
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
     pub_date = models.DateField('date published')
-    link = models.URLField()
-    startup = models.ForeignKey(Startup)
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}:{}".format(self.startup, self.title)
+
+    class Meta:
+        verbose_name = 'news article'
+        ordering = ['-pub_date']
+        get_latest_by = 'pub_date'
